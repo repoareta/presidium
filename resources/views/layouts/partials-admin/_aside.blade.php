3509e4cc3e6f0bc6601fa9d2827b6e79e7b07a3c
sizuwanoadi <adi15siswanto@gmail.com>
2021-07-05 15:15:44 +0700

    add home button at admin

--- resources/views/layouts/partials-admin/_aside.blade.php
+++ resources/views/layouts/partials-admin/_aside.blade.php
@@ -42,6 +42,21 @@
 
 							<!--begin::Menu Nav-->
 							<ul class="menu-nav">
+								<li class="menu-item" aria-haspopup="true">
+									<a href="{{ route('home') }}" class="menu-link">
+										<span class="svg-icon menu-icon">
+											<!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo1/dist/../src/media/svg/icons/Home/Home.svg-->
+											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+												<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+													<rect x="0" y="0" width="24" height="24"/>
+													<path d="M3.95709826,8.41510662 L11.47855,3.81866389 C11.7986624,3.62303967 12.2013376,3.62303967 12.52145,3.81866389 L20.0429,8.41510557 C20.6374094,8.77841684 21,9.42493654 21,10.1216692 L21,19.0000642 C21,20.1046337 20.1045695,21.0000642 19,21.0000642 L4.99998155,21.0000673 C3.89541205,21.0000673 2.99998155,20.1046368 2.99998155,19.0000673 L2.99999828,10.1216672 C2.99999935,9.42493561 3.36258984,8.77841732 3.95709826,8.41510662 Z M10,13 C9.44771525,13 9,13.4477153 9,14 L9,17 C9,17.5522847 9.44771525,18 10,18 L14,18 C14.5522847,18 15,17.5522847 15,17 L15,14 C15,13.4477153 14.5522847,13 14,13 L10,13 Z" fill="#000000"/>
+												</g>
+											</svg>
+											<!--end::Svg Icon-->
+										</span>
+										<span class="menu-text">Home</span>
+									</a>
+								</li>
 								<li class="menu-item {{ Route::is('admin.dashboard') ? 'menu-item-active' : '' }}" aria-haspopup="true">
 									<a href="{{ route('admin.dashboard') }}" class="menu-link">
 										<span class="svg-icon menu-icon">
